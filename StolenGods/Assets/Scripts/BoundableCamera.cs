b9bd2705d24a8b451034434de4046984a99c8890
Garrett Widner <garrettrwidner@gmail.com>
2018-06-28 18:02:07 -0700

    Added crystal attack animation, made mockup level layouts to test level and camera movements, started on BoundableCamera script

---
+++ StolenGods/Assets/Scripts/BoundableCamera.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundableCamera : MonoBehaviour
+{
+    private void Update()
+    {
+        
+    }
+
+    //Just make the camera not be able to go outside the bounds of a bounding box. Otherwise, let it follow the character.
+    //Add functions to be able to call cam to snap unmoving to inside of room, change rooms by lerping, and change to a 
+    //bounded room
+
+
+}
